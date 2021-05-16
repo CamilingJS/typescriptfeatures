fba19edee9100eab7a59a8c9f068c92ed7274a77
Jonathan <jonathancamiling@gmail.com>
2021-05-16 00:09:12 -0700

    creating small variables with annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,10 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true; 
+
+let nothingMuch: null = null; 
+let nonthihng: undefined = undefined; 
+
+//built in objects
+let now: Date = new Date ();
+
---
+++ types.ts
@@ -0,0 +1,17 @@
+//Type annotations: Code we add to tell Typescript what type of value a variable will refer to
+//Type inference: Typescript tries to figure out what type of vlaues a variable refers to
+
+const today = new Date(); 
+today.getMonth();
+
+
+const person = {
+  age: 20
+}
+
+class Color {
+
+}
+
+const red = new Color();
+
